781f789376ebc73dc200057d156c9f0145d0906c
Aman Kumar <amanalphakumar@gmail.com>
2021-11-13 23:49:39 +0530

    Add newDeck()

--- deck.go
+++ deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // 'Deck' extends functionality of []string, analogous to typedef in c++
 type Deck []string
 
+// Func to return a list of cards: a deck
+func newDeck() Deck {
+	cards := Deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Joker", "Queen", "King"}
+
+	// Create combination of cards
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // A receiver (D Deck) on func for 'Deck'
 func (D Deck) printDeck() {
 	for i, card := range D {
