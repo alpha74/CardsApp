8bcc1ff9e19cb3bf980277f63fe81d08304087da
Aman Kumar <amanalphakumar@gmail.com>
2021-11-20 10:24:10 +0530

    Add code to read from file.

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ func main() {
 
 	hand, remainDeck := deal(cards, 3)
 
-	fmt.Println("Saving current Hand to file")
-	hand.saveToFile("currentHand")
 	remainDeck.printDeck()
+
+	// Save hand to file
+	filename := "currentHand"
+	fmt.Println("Saving current Hand to file: ", filename)
+	hand.saveToFile(filename)
+
+	// Read hand from file
+	handFromFile := newDeckFromFile(filename)
+	fmt.Println("\n Hand read from file: ")
+	handFromFile.printDeck()
 }
